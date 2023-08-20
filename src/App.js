2d41d7454309b1a4f95e94eaf19f0b84c9b1155a
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-08-19 22:39:09 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router, Route, Routes,
+} from 'react-router-dom';
+import Home from './Home';
+import Categories from './Categories';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App" />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
