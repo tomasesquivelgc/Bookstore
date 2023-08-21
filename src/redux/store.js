b43f26d37b7b0f376843aa3c9fecd0016fb7bbda
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-08-21 14:30:26 -0300

    Create initial slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import booksReducers from './books/booksSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
 
+    books: booksReducers,
   },
 });
